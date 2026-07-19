c9d28f02ab19bb18a9087f796f05305eed43218f
mayanksuri21 <mayanksurixb18@gmail.com>
2026-07-19 10:58:36 +0530

    Time: 69 ms (82.94%), Space: 131.6 MB (30.66%) - LeetHub

---
+++ LeetCode/Hard/0502-ipo/0502-ipo.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        vector<pair<int,int>> proj;
+        int n = profits.size();
+        for(int i = 0 ; i<profits.size();i++){
+            proj.push_back({capital[i] , profits[i]});
+        };
+        sort(proj.begin() , proj.end());
+
+        priority_queue<int>pq; // max heap
+
+        int idx = 0;
+
+        while(k--){
+            while(idx < n){
+                if(proj[idx].first > w) break;
+
+                pq.push(proj[idx].second);
+                idx++;
+            }
+
+            if(pq.empty()) return w;
+
+            w = w + pq.top();
+            pq.pop();
+        }
+
+        return w;
+
+        
+    }
+};
