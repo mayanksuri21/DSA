0ef5c18bdf93be1bc45ecf81f81ac072b9e0f21c
mayanksuri21 <mayanksurixb18@gmail.com>
2026-08-05 17:36:04 +0530

    Time: 2 ms (20.98%), Space: 9.3 MB (69.05%) - LeetHub

---
+++ LeetCode/Medium/0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        int i , j;
+        vector<vector<int>>dp(m+1 , vector<int>(n+1 , -1));
+        dp[m-1][n-1] = 1;
+        for(j= 0 ; j <= n; j++){
+            dp[m][j] = 0;
+        }
+        for(i = 0 ; i <= m;i++){
+            dp[i][n] = 0;
+
+        }
+        for(i = m-1 ; i >= 0;i--){
+            for(j = n-1 ; j>=0;j--){
+                if (i == m - 1 && j == n - 1)
+                    continue;
+                dp[i][j] = dp[i+1][j] + dp[i][j+1];
+            }
+        }
+
+        return dp[0][0];
+    }
+};
