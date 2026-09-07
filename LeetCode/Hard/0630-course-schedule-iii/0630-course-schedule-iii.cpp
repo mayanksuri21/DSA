17c5e7a0bec2b5968aa822807f1733267e0ea55c
mayanksuri21 <mayanksurixb18@gmail.com>
2026-09-07 21:12:24 +0530

    Time: 62 ms (19.62%), Space: 67.7 MB (18.21%) - LeetHub

---
+++ LeetCode/Hard/0630-course-schedule-iii/0630-course-schedule-iii.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    struct cmp {
+        bool operator()(pair<int,int> &a , pair<int , int> &b){
+            if(a.first != b.first){
+                return a.first > b.first;
+            }
+            return a.second < b.second;
+        }
+    };
+    int scheduleCourse(vector<vector<int>>& courses) {
+                
+        sort(courses.begin(),courses.end(),[](vector<int>&a , vector<int>&b){
+            return a[1] < b[1];
+        });
+
+        priority_queue<int> pq;
+        int count = 0 , totalDuration = 0;
+
+        for(auto course : courses){
+            int duration = course[0];
+            int deadline = course[1];
+
+            pq.push(duration);
+            totalDuration += duration;
+
+            if(totalDuration > deadline){
+                totalDuration -= pq.top();
+                pq.pop();
+            }
+            else{
+                count++;
+
+            }
+
+
+        }
+         
+
+        return count;
+        
+    }
+};
